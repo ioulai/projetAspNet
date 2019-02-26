3d4463607bc718d55fe39664808a871605d5b046
ioulai <oulaiisaac@gmail.com>
2019-02-26 14:39:32 +0100

    initial depot

---
+++ GestionCourse/GestionCourse/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GestionCourse.Startup))]
+namespace GestionCourse
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
