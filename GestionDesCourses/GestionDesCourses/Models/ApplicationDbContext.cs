58b14184044cb8182f2737ee03eda8dcdd2a8cf9
ioulai <oulaiisaac@gmail.com>
2019-02-27 15:24:44 +0100

    correction conflit
    creation d'un nouveau projet

---
+++ GestionDesCourses/GestionDesCourses/Models/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestionDesCourses.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public System.Data.Entity.DbSet<BO.Category> Categories { get; set; }
+
+        public System.Data.Entity.DbSet<BO.DisplayConfiguration> DisplayConfigurations { get; set; }
+
+        public System.Data.Entity.DbSet<BO.UniteDistance> UniteDistances { get; set; }
+    }
+}
---
+++ GestionDesCourses/GestionDesCourses/Models/RaceViewModel.cs
@@ -0,0 +1,18 @@
+﻿using BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GestionDesCourses.Models
+{
+    public class RaceViewModel
+    {
+        public Race Race { get; set; }
+
+        public List<SelectListItem> Categories { get; set; } = new List<SelectListItem>();
+
+        public int? IdSelectedCategory { get; set; }
+    }
+}
