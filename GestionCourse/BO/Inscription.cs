58b6640d611889d8aeb9bff2bf6268214128c535
Morgane <morgane.ondoobame2016@campus-eni.fr>
2019-02-27 09:23:17 +0100

    MOO : ajout public et Race

--- GestionCourse/BO/Inscription.cs
+++ GestionCourse/BO/Inscription.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BO
 {
-    class Inscription
+    public class Inscription
     {
     }
 }
--- GestionCourse/BO/TypeInscription.cs
+++ GestionCourse/BO/TypeInscription.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BO
 {
-    class TypeInscription
+    public class TypeInscription
     {
     }
 }
