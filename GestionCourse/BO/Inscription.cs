89e31e64f5a38d8b3beba3535b6d4deb0e6e449c
Morgane <morgane.ondoobame2016@campus-eni.fr>
2019-02-27 09:53:33 +0100

    MOO : update Inscriptions

--- GestionCourse/BO/Inscription.cs
+++ GestionCourse/BO/Inscription.cs
@@ -8,5 +8,16 @@ namespace BO
 {
     public class Inscription
     {
+        public int Id { get; set; } // l'id de l'inscription
+
+        public float Amount { get; set; } // ??? 
+
+        public int IdentityModelId { get; set; } // l'id de la personne qui s'inscrit
+
+        public int RaceId { get; set; } // l'id de la course  laquelle la personne s'inscrit
+
+        public int TypeInscriptionId { get; set; } // // l'id correspondant au type d'inscription (la personne s'inscrit en tant que compétiteur ou organisateur)
+
+        // atributs à étudier : "Number", "Positions" -> correspondent à quoi?
     }
 }
