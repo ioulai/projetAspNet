cd08914f97431bfdf939f7dffbdb7c4c9ef1584a
maklourson <36851730+maklourson@users.noreply.github.com>
2019-02-27 12:06:26 +0100

    ignore fichier

--- GestionCourse/GestionCourse/Models/ContextGestionCourse.cs
+++ GestionCourse/GestionCourse/Models/ContextGestionCourse.cs
@@ -8,5 +8,9 @@ namespace GestionCourse.Models
 {
     public class ContextGestionCourse : DbContext
     {
+        public System.Data.Entity.DbSet<BO.Category> Categories { get; set; }
+        public System.Data.Entity.DbSet<BO.UniteDistance> UniteDistance { get; set; }
+        public System.Data.Entity.DbSet<BO.DisplayConfiguration> DisplayConfigurations { get; set; }
+
     }
 }
