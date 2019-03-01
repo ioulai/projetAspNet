be4a15e9501cc6e32e32c89304426bb9cccfbdaf
Morgane <morgane.ondoobame2016@campus-eni.fr>
2019-03-01 12:09:44 +0100

    MOO : gestion validation courses

--- BO/Race.cs
+++ BO/Race.cs
@@ -14,24 +14,32 @@ namespace BO
         [Display(Name = "Catégorie")]
         public Category Category { get; set; } //virtual??
 
+        [Required]
         [Display(Name = "Date de fin")]
         public DateTime DateEnd { get; set; }
 
+        [Required]
         [Display(Name = "Date de début")]
         public DateTime DateStart { get; set; }
 
+        [Required]
+        [StringLength(200, MinimumLength = 5, ErrorMessage = "La Description doit contenir entre 5 et 200 caractères")]
         public string Description { get; set; }
 
         public List<Inscription> Inscriptions { get; set; }
 
         public List<Poi> Pois { get; set; }
 
+        [Required]
         [Display(Name = "Prix")]
         public float Price { get; set; }
 
+        [Required]
+        [StringLength(25, MinimumLength = 5, ErrorMessage = "Le Titre doit contenir entre 5 et 25 caractères")]
         [Display(Name = "Titre")]
         public string Title { get; set; }
 
+        [Required]
         [Display(Name = "Code postal")]
         public string ZipCode { get; set; }
     }
--- GestionDesCourses/GestionDesCourses/Controllers/RacesController.cs
+++ GestionDesCourses/GestionDesCourses/Controllers/RacesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -16,16 +17,13 @@ namespace GestionDesCourses.Controllers
         private ApplicationDbContext db = new ApplicationDbContext();
 
         private static List<Category> lesCategoriesDispo;
-        private static IEnumerable<Race> lesCourses;
-
 
         public RacesController()
         {
             if(lesCategoriesDispo == null)
             {
                 lesCategoriesDispo = db.Categories.ToList();
-            }
-            
+            }          
         }
 
         // GET: Races
@@ -53,7 +51,7 @@ namespace GestionDesCourses.Controllers
         // GET: Races/Create
         public ActionResult Create()
         {
-            // cration du ViewModel nécessaire pour porter la liste des catégories et l'id de la categorie choisie en plus de la course
+            // création du ViewModel nécessaire pour porter la liste des catégories et l'id de la categorie choisie en plus de la course
             var raceVM = new RaceViewModel();
             raceVM.Categories = lesCategoriesDispo;
 
@@ -67,24 +65,41 @@ namespace GestionDesCourses.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(RaceViewModel raceVM)
         {
+            
             if (ModelState.IsValid)
             {
-                // on vérifie qu'une catégorie ai été choisie
-                if (raceVM.IdSelectedCategory.HasValue)
+                try
                 {
-                    // on assigne la catégorie dont l'Id à été choisi pour la course
-                    raceVM.Race.Category = db.Categories.FirstOrDefault(a => a.Id == raceVM.IdSelectedCategory.Value);
-                }
+                    // on vérifie la validité des données
+                    if (!CheckRules(raceVM))
+                    {
+                        throw new Exception("Une ou plusieurs rêgles métiers ne sont pas respectées sur une création de course");
+                    }
 
-                // On ajoute la nouvelle course au DbSet, puis on enregistre les changements en base
-                db.Races.Add(raceVM.Race);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                    // on vérifie qu'une catégorie ai été choisie
+                    if (raceVM.IdSelectedCategory.HasValue)
+                    {
+                        // on assigne la catégorie dont l'Id à été choisi pour la course
+                        raceVM.Race.Category = db.Categories.FirstOrDefault(a => a.Id == raceVM.IdSelectedCategory.Value);
+                    }
 
+                    // On ajoute la nouvelle course au DbSet, puis on enregistre les changements en base
+                    db.Races.Add(raceVM.Race);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
                 
+                }
+                
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    // Si nous avons rencontré une erreur, il faut recharger la page de création, de ce fait il nous faut ré-alimenter le Viewmodel avant de le passer à la vue
+                    raceVM.Categories = lesCategoriesDispo;
+                    return View(raceVM);
+                }
             }
             raceVM.Categories = lesCategoriesDispo;
-            return View(raceVM);
+            return View(raceVM);  
         }
 
         // GET: Races/Edit/5
@@ -128,26 +143,43 @@ namespace GestionDesCourses.Controllers
                 return RedirectToAction("Index");
             }
             return View(raceVM);
-*/
+        */
             if (ModelState.IsValid)
             {
-                var racedb = db.Races.Include(z => z.Category).FirstOrDefault(i => i.Id == raceVM.Race.Id);
-                racedb.Title = raceVM.Race.Title;
-                racedb.Category = null;
-                if (raceVM.IdSelectedCategory.HasValue)
+                try
                 {
-                    racedb.Category = db.Categories.FirstOrDefault(a => a.Id == raceVM.IdSelectedCategory.Value);
+                    // on vérifie la validité des données
+                    if (!CheckRules(raceVM))
+                    {
+                        throw new Exception("Une ou plusieurs rêgles métiers ne sont pas respectées sur une création de course");
+                    }
+
+                    var racedb = db.Races.Include(z => z.Category).FirstOrDefault(i => i.Id == raceVM.Race.Id);
+                    racedb.Title = raceVM.Race.Title;
+                    racedb.Category = null;
+                    if (raceVM.IdSelectedCategory.HasValue)
+                    {
+                        racedb.Category = db.Categories.FirstOrDefault(a => a.Id == raceVM.IdSelectedCategory.Value);
+                    }
+                    racedb.DateStart = raceVM.Race.DateStart;
+                    racedb.DateEnd = raceVM.Race.DateEnd;
+                    racedb.Description = raceVM.Race.Description;
+                    racedb.Price = raceVM.Race.Price;
+                    racedb.ZipCode = raceVM.Race.ZipCode;
+                    // pareil pour inscriptions etc...
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
                 }
-                racedb.DateStart = raceVM.Race.DateStart;
-                racedb.DateEnd = raceVM.Race.DateEnd;
-                racedb.Description = raceVM.Race.Description;
-                racedb.Price = raceVM.Race.Price;
-                racedb.ZipCode = raceVM.Race.ZipCode;
-                // pareil pour inscriptions etc...
-                db.SaveChanges();
-                return RedirectToAction("Index");
+
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    // Si nous avons rencontré une erreur, il faut recharger la page de création, de ce fait il nous faut ré-alimenter le Viewmodel avant de le passer à la vue
+                    raceVM.Categories = lesCategoriesDispo;
+                    return View(raceVM);
+                }
             }
-            raceVM.Categories = db.Categories.ToList(); //???
+            raceVM.Categories = lesCategoriesDispo;
             return View(raceVM);
             
         }
@@ -185,6 +217,53 @@ namespace GestionDesCourses.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        private bool CheckRules(RaceViewModel raceVm)
+        {
+            var brokenRules = 0;
+            // Rq : pour les champs obligatoires (tous en fait) c'est géré avec le "[Required]" dans les champs de la classe BO>Race
+            // idem pour la taille des chaines de caractère
+
+            // le titre doit etre unique
+            List<Race> coursesExistantes = db.Races.ToList();
+            if (coursesExistantes.Any(p => p.Title.ToUpper() == raceVm.Race.Title.ToUpper() && p.Id != raceVm.Race.Id))
+            {
+                ModelState.AddModelError("Race.Title", "Il existe déjà une course portant ce titre");
+                brokenRules++;
+            }
+
+            // vérifier que la date de début soit bien antérieure à la date de fin
+            if (raceVm.Race.DateStart > raceVm.Race.DateEnd)
+            {
+                ModelState.AddModelError("Race.DateEnd", "La date de fin doit etre postérieure à la date de début");
+                ModelState.AddModelError("Race.DateStart", "La date de début doit etre antérieure à la date de fin");
+                brokenRules++;
+            }
+
+            // vérifier que la date de début soit bien postérieure à la date du jour
+            if (raceVm.Race.DateStart < DateTime.Now)
+            {
+                ModelState.AddModelError("Race.DateStart", "La date de début doit etre postérieure à la date du jour");
+                brokenRules++;
+            }
+
+            // vérifier que la date de fin soit bien postérieure à la date du jour
+            if (raceVm.Race.DateEnd < DateTime.Now)
+            {
+                ModelState.AddModelError("Race.DateEnd", "La date de fin doit etre postérieure à la date du jour");
+                brokenRules++;
+            }
+
+            // il faut des POI (au moins deux : départ et arrivée)
+            /*          if (raceVm.IdSelectedPois.Count < 2)
+                        {
+                            ModelState.AddModelError("", "Une course doit avoir au moins deux points d'interets");
+                            brokenRules++;
+                        }
+            */
+
+            return brokenRules == 0;
         }
     }
 }
